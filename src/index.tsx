82cb6c486e703a91aa285cfe2191ed87b10256a7
DawonOh <dawonohh@gmail.com>
2023-01-10 03:10:58 +0900

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import 'Styles/reset.scss';
+import 'Styles/common.scss';
+import Router from 'Router';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>
 );
 
