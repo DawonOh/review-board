6438a2a3a9025e5e7a0a50dd1e795135635adf59
DawonOh <dawonohh@gmail.com>
2023-01-28 03:00:41 +0900

    Modify: react-modal을 styled-react-modal로 변경 - 반응형 레이아웃을 위한 react-responsive 추가

--- src/Components/AlertModal/index.tsx
+++ src/Components/AlertModal/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Styled from 'styled-components';
-import Modal from 'react-modal';
+import Modal, { BaseModalBackground, ModalProvider } from 'styled-react-modal';
 
 import { Button } from 'Components';
 import { flexCenterAlign } from 'Styles/CommonStyle';
@@ -20,6 +20,17 @@ const AlertMessage = Styled.div`
   gap: 0.3rem;
 `;
 
+const StyledModal = Modal.styled`
+  width: 20rem;
+  height: 12rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+  background-color: #fff;
+  border-radius: 4px;
+`;
+
 interface Props {
   isAlertModalOpen: boolean;
   setIsAlertModalOpen: (isAlertModalOpen: boolean) => void;
@@ -31,56 +42,30 @@ export const AlertModal = ({
   setIsAlertModalOpen,
   contents,
 }: Props) => {
-  Modal.setAppElement('#root');
   return (
-    <Modal
-      isOpen={isAlertModalOpen}
-      onRequestClose={() => setIsAlertModalOpen(false)}
-      style={{
-        overlay: {
-          width: '100%',
-          height: '100%',
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          backgroundColor: 'rgba(0,0,0, 0.4)',
-          zIndex: 999,
-        },
-        content: {
-          position: 'absolute',
-          width: '22rem',
-          height: '12rem',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          border: '1px solid #ccc',
-          background: '#fff',
-          overflow: 'auto',
-          WebkitOverflowScrolling: 'touch',
-          borderRadius: '6px',
-          outline: 'none',
-          padding: '20px',
-        },
-      }}
-    >
-      <AlertModalContainer>
-        <AlertMessage>
-          {contents.map(content => {
-            return <p key={content.id}>{content.text}</p>;
-          })}
-        </AlertMessage>
-        <Button
-          content="확인"
-          backgroundColor="#676FA3"
-          color="#fff"
-          size="0.4rem 0.6rem"
-          onClick={() => {
-            setIsAlertModalOpen(false);
-          }}
-        />
-      </AlertModalContainer>
-    </Modal>
+    <ModalProvider>
+      <StyledModal
+        isOpen={isAlertModalOpen}
+        onBackgroundClick={() => setIsAlertModalOpen(false)}
+        onEscapeKeydown={() => setIsAlertModalOpen(false)}
+      >
+        <AlertModalContainer>
+          <AlertMessage>
+            {contents.map(content => {
+              return <p key={content.id}>{content.text}</p>;
+            })}
+          </AlertMessage>
+          <Button
+            content="확인"
+            backgroundColor="#676FA3"
+            color="#fff"
+            size="0.4rem 0.6rem"
+            onClick={() => {
+              setIsAlertModalOpen(false);
+            }}
+          />
+        </AlertModalContainer>
+      </StyledModal>
+    </ModalProvider>
   );
 };
--- src/Components/Login/index.tsx
+++ src/Components/Login/index.tsx
@@ -1,6 +1,7 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import Styled from 'styled-components';
-import Modal from 'react-modal';
+import Modal, { ModalProvider } from 'styled-react-modal';
+import { useMediaQuery } from 'react-responsive';
 
 import { Button, AlertModal } from 'Components';
 import closeButtonImg from '../../assets/images/close.png';
@@ -10,6 +11,7 @@ import { Link } from 'react-router-dom';
 const LoginModalContainer = Styled.div`
   width: 100%;
   height: 100%;
+  position: relative;
   ${flexCenterAlign}
   flex-direction: column;
   gap: 1.2rem;
@@ -51,6 +53,17 @@ const Buttons = Styled.div`
 const GoToJoin = Styled.span`
   font-size: 0.9rem;
 `;
+
+const StyledModal = Modal.styled`
+  width: 32rem;
+  height: 20rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  border-radius: 4px;
+`;
+
 interface Props {
   isModalOpen: boolean;
   setIsModalOpen: (isModalOpen: boolean) => void;
@@ -60,6 +73,7 @@ interface MessageType {
   id: number;
   text: string;
 }
+
 export const Login = ({ isModalOpen, setIsModalOpen }: Props) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -70,7 +84,8 @@ export const Login = ({ isModalOpen, setIsModalOpen }: Props) => {
   const BACK_URL = process.env.REACT_APP_BACK_URL;
   const BACK_PORT = process.env.REACT_APP_BACK_DEFAULT_PORT;
 
-  Modal.setAppElement('#root');
+  const isDesktop = useMediaQuery({ minDeviceWidth: 1024 });
+  const isTabletOrMobile = useMediaQuery({ maxDeviceWidth: 1023 });
 
   const getEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
@@ -91,99 +106,87 @@ export const Login = ({ isModalOpen, setIsModalOpen }: Props) => {
     }
   }, [email, password]);
   const login = () => {
-    fetch(`${BACK_URL}:${BACK_PORT}/users/signin`, {
-      method: 'POST',
-      headers: requestHeaders,
-      body: JSON.stringify({
-        email: email,
-        password: password,
-      }),
-    })
-      .then(res => res.json())
-      .then(json => {
-        let message = String(json.message);
-        if (message.includes('success')) {
-          localStorage.setItem('token', json.result.token);
-          setIsModalOpen(false);
-        } else {
-          setIsAlertModalOpen(true);
-          setMessages([
-            { id: 1, text: '이메일/비밀번호가 일치하지 않습니다.' },
-          ]);
-          setEmail('');
-          setPassword('');
-        }
-      });
+    // fetch(`${BACK_URL}:${BACK_PORT}/users/signin`, {
+    //   method: 'POST',
+    //   headers: requestHeaders,
+    //   body: JSON.stringify({
+    //     email: email,
+    //     password: password,
+    //   }),
+    // })
+    //   .then(res => res.json())
+    //   .then(json => {
+    //     let message = String(json.message);
+    //     if (message.includes('success')) {
+    //       localStorage.setItem('token', json.result.token);
+    //       setIsModalOpen(false);
+    //     } else {
+    //       setIsAlertModalOpen(true);
+    //       setMessages([
+    //         { id: 1, text: '이메일/비밀번호가 일치하지 않습니다.' },
+    //       ]);
+    //       setEmail('');
+    //       setPassword('');
+    //     }
+    //   });
+    setIsAlertModalOpen(true);
+    setMessages([{ id: 1, text: '이메일/비밀번호가 일치하지 않습니다.' }]);
+    setEmail('');
+    setPassword('');
+  };
+
+  const openAlertModal = () => {
+    if (isAlertModalOpen) {
+      return (
+        <AlertModal
+          isAlertModalOpen={isAlertModalOpen}
+          setIsAlertModalOpen={setIsAlertModalOpen}
+          contents={messages}
+        />
+      );
+    }
   };
 
   return (
     <Fragment>
-      <AlertModal
-        isAlertModalOpen={isAlertModalOpen}
-        setIsAlertModalOpen={setIsAlertModalOpen}
-        contents={messages}
-      />
-      <Modal
-        isOpen={isModalOpen}
-        onRequestClose={() => setIsModalOpen(false)}
-        style={{
-          overlay: {
-            width: '100%',
-            height: '100%',
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            backgroundColor: 'rgba(0,0,0, 0.4)',
-          },
-          content: {
-            position: 'absolute',
-            width: '38rem',
-            height: '25rem',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            border: '1px solid #ccc',
-            background: '#fff',
-            overflow: 'auto',
-            WebkitOverflowScrolling: 'touch',
-            borderRadius: '6px',
-            outline: 'none',
-            padding: '20px',
-          },
-        }}
-      >
-        <LoginModalContainer>
-          <CloseButton onClick={() => setIsModalOpen(false)} />
-          <Title>Title</Title>
-          <Input
-            type="text"
-            placeholder="이메일을 입력해주세요."
-            onChange={getEmail}
-            value={email}
-          />
-          <Input
-            type="text"
-            placeholder="비밀번호를 입력해주세요."
-            onChange={getPw}
-            value={password}
-          />
-          <Buttons>
-            <Button
-              content="로그인"
-              backgroundColor={email && password ? '#FF5959' : '#E0E0E0'}
-              size="0.4rem 7rem"
-              color="#fff"
-              onClick={login}
-              disabled={isDisabled}
+      <ModalProvider>
+        <StyledModal
+          isOpen={isModalOpen}
+          onBackgroundClick={() => setIsModalOpen(false)}
+          onEscapeKeydown={() => setIsModalOpen(false)}
+        >
+          <LoginModalContainer>
+            <CloseButton onClick={() => setIsModalOpen(false)} />
+            <Title>Title</Title>
+            <Input
+              type="text"
+              placeholder="이메일을 입력해주세요."
+              onChange={getEmail}
+              value={email}
             />
-          </Buttons>
-          <Link to="/join">
-            <GoToJoin>회원가입</GoToJoin>
-          </Link>
-        </LoginModalContainer>
-      </Modal>
+            <Input
+              type="password"
+              placeholder="비밀번호를 입력해주세요."
+              onChange={getPw}
+              value={password}
+            />
+            <Buttons>
+              <Button
+                content="로그인"
+                backgroundColor={email && password ? '#FF5959' : '#E0E0E0'}
+                size="0.4rem 7rem"
+                color="#fff"
+                onClick={login}
+                disabled={isDisabled}
+              />
+            </Buttons>
+            <Link to="/join">
+              <GoToJoin>회원가입</GoToJoin>
+            </Link>
+          </LoginModalContainer>
+        </StyledModal>
+      </ModalProvider>
+      {openAlertModal()}
     </Fragment>
   );
 };
