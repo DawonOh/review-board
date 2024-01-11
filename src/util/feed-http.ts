66d184386c189858efd108debb8c623dba4a89a0
DawonOh <dawonohh@gmail.com>
2024-01-12 06:49:18 +0900

    Modify: 댓글 정보 불러오기 실패 시 에러 메세지 수정

--- src/util/feed-http.ts
+++ src/util/feed-http.ts
@@ -142,7 +142,10 @@ export const feedComments = async ({
     const response = await instance.get(`/comments/${feedId}`);
     return response.data;
   } catch (error) {
-    throw error;
+    throw json(
+      { message: '정보를 불러오지 못했습니다. 잠시 후 다시 시도해주세요.' },
+      { status: 500 }
+    );
   }
 };
 
